07675334592b08e4b78eb926f3ba52ab40789ea8
AMac <amac@AMacs-MacBook-Pro.local>
2018-08-07 15:01:25 +0300

    Thymeleaf layouts added

--- real-estate-app/src/main/java/com/realestateapp/realestateapp/controllers/HomeController.java
+++ real-estate-app/src/main/java/com/realestateapp/realestateapp/controllers/HomeController.java
@@ -1,16 +1,46 @@
 package com.realestateapp.realestateapp.controllers;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-@EnableAutoConfiguration
 public class HomeController {
-    @GetMapping("")
-    public String index() {
-        return "index";
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("view","home/index");
+
+        return "base";
     }
+
+    @GetMapping("/about")
+    public String aboutPage(Model model) {
+        model.addAttribute("view","company/about");
+
+        return "base";
+    }
+
+    @GetMapping("/contact")
+    public String contactPage(Model model) {
+        model.addAttribute("view","others/contact");
+
+        return "base";
+    }
+
+    @GetMapping("/terms-and-conditions")
+    public String termsPage(Model model) {
+        model.addAttribute("view","others/terms");
+
+        return "base";
+    }
+
+    @GetMapping("/faqs")
+    public String faqs(Model model) {
+        model.addAttribute("view","others/faqs");
+
+        return "base";
+    }
+
 }
